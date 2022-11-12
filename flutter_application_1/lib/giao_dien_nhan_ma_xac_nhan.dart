401170960665576a1d26b71b1e0d3cf26ffb9fd9
TranKhanh1905 <tranquockhanh190502@gmail.com>
2022-11-13 00:41:30 +0700

    Them giao dien nhan ma xac thuc

---
+++ flutter_application_1/lib/giao_dien_nhan_ma_xac_nhan.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        home: Scaffold(
+            body: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/h1.jpg'),
+                  fit: BoxFit.cover
+                )
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(90.0),
+                      color: Colors.white
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(90.0),
+                        ),
+                        labelText: 'EMAIL',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(90.0),
+                      color: Colors.white
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(90.0),
+                        ),
+                        labelText: 'OTP',
+                      ),
+                    ),
+                  ),
+                  Container(
+                      height: 80,
+                      padding: const EdgeInsets.fromLTRB(150, 10, 150, 30),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(50),
+                        ),
+                        child: const Text(
+                          'SEND', 
+                          style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
+                          ),
+                        onPressed: () {},
+                      )),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Quên mật khẩu?',
+                      style: TextStyle(color: Colors.white, backgroundColor: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            )));
+  }
+}
